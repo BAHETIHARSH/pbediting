d1cda3dfc70a6823df0649eb0ca8be1202ef5efd
Harsh Balaprasad Baheti <65105435+BAHETIHARSH@users.noreply.github.com>
2020-10-23 15:31:42 +0530

    index

---
+++ index/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Contact, Blog, Tutorial
+
+# Register your models here.
+# this is superuser studio,hari and pass is Harsh@1256 Harsh@1256
+admin.site.register(Contact)
+admin.site.register(Blog)
+admin.site.register(Tutorial)
